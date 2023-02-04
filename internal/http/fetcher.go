278363a82af819117fa5dcb82ee4cb2a2a7efa0e
Abdussamet Kocak <abdus@abdus.dev>
2023-02-04 17:41:40 +0100

    feat(internal/http): Log requests

--- internal/http/fetcher.go
+++ internal/http/fetcher.go
@@ -3,6 +3,8 @@ package http
 import (
 	"errors"
 	"fmt"
+
+	"github.com/rs/zerolog/log"
 )
 
 type htmlFetcher struct {
@@ -11,10 +13,12 @@ type htmlFetcher struct {
 var ErrNotFound = errors.New("page not found")
 
 func (s htmlFetcher) FetchHTML(url string) (string, error) {
+	log.Debug().Str("url", url).Msg("fetching url")
 	res, err := DefaultClient.R().Get(url)
 	if err != nil {
 		return "", fmt.Errorf("failed to get url: %w", err)
 	}
+	log.Debug().Int("status_code", res.StatusCode).Str("url", res.Request.RawURL).Msg("got response")
 	if res.IsErrorState() {
 		return "", ErrNotFound
 	}
