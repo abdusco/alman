5cec8e078013afaf5f18fc6ab68e6a06681e4758
Abdussamet Kocak <abdus@abdus.dev>
2023-03-14 13:45:32 +0100

    fix: Allow user agent to be configured via an environment variable

--- internal/http/client.go
+++ internal/http/client.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"os"
 	"time"
 
 	"github.com/imroc/req/v3"
@@ -8,5 +9,14 @@ import (
 
 var DefaultClient = req.NewClient().
 	SetTimeout(time.Second * 10).
+	SetUserAgent(envOrDefault("USER_AGENT", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")).
 	DisableAutoReadResponse().
 	EnableInsecureSkipVerify()
+
+func envOrDefault(name string, defaultValue string) string {
+	value, ok := os.LookupEnv(name)
+	if !ok {
+		value = defaultValue
+	}
+	return value
+}
